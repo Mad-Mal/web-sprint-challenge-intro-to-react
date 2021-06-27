b53491f7ef5b23bcd40ed5b85a2d1c9ed7eaa487
Mallory <mallorydoescoding@gmail.com>
2021-06-26 21:59:27 -0500

    Base project up and going, still need to add styling to page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import Character from './components/Character'
 
 const App = () => {
+  const [characterList, setCharacterList] = useState([]);
+
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get('https://swapi.dev/api/people')
+          .then ((data) => {
+            console.log(data)
+            setCharacterList(data.data)
+          })
+          .catch(err => {
+            console.log(err)
+          })
+    }
+    getData();
+  }, [])
+
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
@@ -12,6 +32,7 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character props={characterList}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,24 @@
-// Write your Character component here
+import react from 'react'
+import styled from 'styled-components';
+
+
+function Character(props) {
+
+    const characterInfo = props.props;
+
+    return (
+            characterInfo.map(info => {
+                return (
+                    <divWrap>
+                        <p>{info.name}</p>
+                        <p>Gender: {info.gender}</p>
+                        <p>Height: {info.height}cm</p>
+                        <p>Mass: {info.mass} grams</p>
+                        <p>Birth Year: {info.birth_year}</p>
+                    </divWrap>
+                );
+            })
+    );
+}
+
+export default Character;
